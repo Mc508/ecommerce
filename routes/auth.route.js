aed3c835998aef6250c062956895c72e7b82efbe
Mc508 <mchauhan178@rku.ac.in>
2024-03-16 08:33:47 +0530

    Login with mw

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -9,6 +9,6 @@ module.exports=(app)=>{
     app.post('/ecomm/api/v1/auth/signup',[authMw.verifySignUpBody],authController.signup)
     
     /**route.for signin */
-    app.post('/ecomm/api/v1/auth/signin',authController.signin)
+    app.post('/ecomm/api/v1/auth/signin',[authMw.verifySignInBody],authController.signin)
 }
 
