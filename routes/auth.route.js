310656d6757786496a9c87d6b2a24c8d48443569
Mc508 <mchauhan178@rku.ac.in>
2024-03-16 08:23:28 +0530

    signin api

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,4 +7,8 @@ const authMw = require("../middlewares/auth_mw")
 
 module.exports=(app)=>{
     app.post('/ecomm/api/v1/auth/signup',[authMw.verifySignUpBody],authController.signup)
+    
+    /**route.for signin */
+    app.post('/ecomm/api/v1/auth/signin',authController.signin)
 }
+
